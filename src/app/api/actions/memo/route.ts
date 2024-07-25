68feff6e94bf99f1e340cac924476c8a623c5c47
jicodes <liu.ji1@northeastern.edu>
2024-07-25 16:38:23 -0700

    add POST endpoint that can return a signable base64 encoded serialized transaction

--- src/app/api/actions/memo/route.ts
+++ src/app/api/actions/memo/route.ts
@@ -1,4 +1,21 @@
-import { ActionGetResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
+import {
+  ActionGetResponse,
+  ActionPostRequest,
+  ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+  createPostResponse,
+  MEMO_PROGRAM_ID,
+} from "@solana/actions";
+import {
+  clusterApiUrl,
+  ComputeBudgetProgram,
+  Connection,
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import cluster from "cluster";
+import { create } from "domain";
 
 export const GET = (req: Request) => {
   const payload: ActionGetResponse = {
@@ -15,3 +32,58 @@ export const GET = (req: Request) => {
 
 // mock options method to allow CORS preflight requests
 export const OPTIONS = GET;
+
+// Return a signable transaction for the user to sign
+export const POST = async (req: Request) => {
+  try {
+    // Parse the request body
+    const body: ActionPostRequest = await req.json();
+
+    // Validate the request body
+    let account: PublicKey;
+    try {
+      account = new PublicKey(body.account);
+    } catch (error) {
+      return new Response("Invalid 'account'", {
+        status: 400,
+        headers: ACTIONS_CORS_HEADERS,
+      });
+    }
+
+    // Create a new transaction
+    const transaction = new Transaction();
+    transaction.add(
+      // note: `createPostResponse` repuires at least 1 non-memo instruction
+      ComputeBudgetProgram.setComputeUnitPrice({
+        microLamports: 1000,
+      }),
+
+      new TransactionInstruction({
+        programId: new PublicKey(MEMO_PROGRAM_ID),
+        data: Buffer.from("Hello, Solana! 🚀", "utf-8"),
+        keys: [],
+      }),
+    );
+
+    // set the fee payer of the transaction
+    transaction.feePayer = account;
+
+    const connection = new Connection(clusterApiUrl("devnet"));
+    // Set transaction recent blockhash
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
+    // craft the response payload
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        transaction,
+      },
+      // signers: [],
+    });
+
+    return Response.json(payload, { headers: ACTIONS_CORS_HEADERS });
+  } catch (error) {
+    return Response.json("An unknown error occured", { status: 400 });
+  }
+};
